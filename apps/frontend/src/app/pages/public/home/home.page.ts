94daf2d4d78db8825005dc0ecaa853074eb48703
Dansteve <danstevea@gmail.com>
2023-08-11 17:00:25 +0100

    [PHD-23] Home

--- apps/frontend/src/app/pages/public/home/home.page.ts
+++ apps/frontend/src/app/pages/public/home/home.page.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { environment } from '../../../../environments/environment';
+import { ApiService } from '../../../services/api/api.service';
+import { HelperMethodsService } from '../../../services/helper-methods/helper-methods.service';
+import { ScreenSizeService } from '../../../services/screen-size/screen-size.service';
 
 @Component({
   selector: 'peachy-healthcare-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  showPassword = false;
+  isLoading = false;
+
+  readonly isPwa: Readonly<boolean> = environment.isPwa;
+  protected isDesktop!: boolean;
+
+  constructor(
+    public screenSizeService: ScreenSizeService,
+    public navController: NavController,
+    public router: Router,
+    public formBuilder: FormBuilder,
+    public apiService: ApiService,
+    public helperMethods: HelperMethodsService
+  ) {
+    this.screenSizeService.isDesktopView().subscribe(isDesktop => {
+      this.isDesktop = isDesktop;
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  goTo(page = '') {
+    const link = `/public/${page}`;
+    // this.router.navigate([link],);
+    this.navController.navigateForward(link, { animated: false });
+  }
 }
