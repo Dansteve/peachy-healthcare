85c2e0bbaa5708781759fdb62add3018d909578b
Dansteve <danstevea@gmail.com>
2023-08-12 19:30:58 +0100

    [PHD-33] Profile

---
+++ apps/frontend/src/app/pages/member/profile/profile.page.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'peachy-healthcare-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage {
+  constructor() {}
+}
