a36a887c04cfa9efe15e4b1c552adb39c05b6594
Dansteve <danstevea@gmail.com>
2023-08-12 19:31:21 +0100

    [PHD-34] Dashboard

---
+++ apps/frontend/src/app/pages/member/dashboard/dashboard.page.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'peachy-healthcare-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage {
+  constructor() {}
+}
