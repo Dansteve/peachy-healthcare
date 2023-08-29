f8988fbaec3d926ae6c3b091f979a587e21d8251
Dansteve <danstevea@gmail.com>
2023-08-29 11:06:13 +0100

    [PHD-48] Update Images

--- apps/frontend/src/app/components/shared-components.module.ts
+++ apps/frontend/src/app/components/shared-components.module.ts
@@ -6,6 +6,8 @@ import { MomentModule } from 'ngx-moment';
 import { NgPipesModule } from 'ngx-pipes';
 import { SharedPipeModule } from '../pipes/shared-pipe.module';
 import { GenericHeaderComponent } from './generic-header/generic-header.component';
+import { TakeTestComponent } from './take-test/take-test.component';
+import { TestCongratulationsComponent } from './test-congratulations/test-congratulations.component';
 
 @NgModule({
   imports: [
@@ -18,9 +20,13 @@ import { GenericHeaderComponent } from './generic-header/generic-header.componen
   ],
   declarations: [
     GenericHeaderComponent,
+    TakeTestComponent,
+    TestCongratulationsComponent
   ],
   exports: [
     GenericHeaderComponent,
+    TakeTestComponent,
+    TestCongratulationsComponent
   ]
 })
 export class SharedComponentsModule { }
